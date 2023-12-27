156f6181942760021a90e036221cb98a80f5c3f7
Edith Rand <edithrand505@gmail.com>
2023-12-27 09:39:46 +0100

    abreviation

---
+++ Abreviation.js
@@ -0,0 +1,13 @@
+function abreviation(nomComplet) {
+    
+    // Séparer le prénom et le nom
+    const parties = nomComplet.split(' ');
+    // Récupérer le prénom et l'initial du nom
+    const prenom = parties[0];
+    const initialNom = parties[1].charAt(0).toUpperCase();  
+    // Retourner l'abréviation
+    return `${prenom} ${initialNom}.`;
+}
+
+// Exemple d'utilisation
+console.log( abreviation("John Doe Rakoto"));
