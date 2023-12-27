d32dbce6c1691d2047fc621964c4329ec3572c2e
Edith Rand <edithrand505@gmail.com>
2023-12-27 08:56:12 +0100

    git version

--- inversion.js
+++ inversion.js
@@ -1,15 +1,22 @@
-function inversion(str){
-    return (str.split('').reverse().join(''))
+// Fonction pour inverser une chaîne de caractères
+function inversion(str) {
+    // Utilisation de split pour diviser la chaîne en un tableau de caractères,
+    // reverse pour inverser l'ordre des caractères, et join pour reformer la chaîne inversée
+    return str.split('').reverse().join('');
 }
 
-function palindrome(mots){
-    let strMin = mots.toLowerCase()
-    if(inversion(strMin) === strMin){
-        console.log(`${mots} est un palindrome`)
-    }
-    else{
-        console.log(`${mots} n'est pas un palindrome`)
+// Fonction pour vérifier si un mot est un palindrome
+function palindrome(mots) {
+    // Convertir le mot en minuscules pour une comparaison insensible à la casse
+    let strMin = mots.toLowerCase();
+
+    // Appel de la fonction inversion pour obtenir la version inversée du mot
+    if (inversion(strMin) === strMin) {
+        console.log(`${mots} est un palindrome`);
+    } else {
+        console.log(`${mots} n'est pas un palindrome`);
     }
 }
 
-palindrome('Elle')
+// Appel de la fonction palindrome avec un exemple
+palindrome('Elle');
--- voyelle.js
+++ voyelle.js
@@ -1,27 +1,48 @@
-//La méthode includes() permet de déterminer si un tableau contient une valeur et renvoie true si c'est le cas, false sinon.
-function getVoyelleNumber(phrase){
+// Fonction principale pour compter le nombre de voyelles dans une phrase
+function getVoyelleNumber(phrase) {
+    // Définition des voyelles
+    const voyelles = ['a', 'e', 'i', 'y', 'o', 'u'];
 
-    //Declaré dans un tableau tout les voyelles(qui a toujours besoin de '' car c'est un string)
-    const voyelle = ['a', 'e', 'i' , 'y', 'o', 'u']
-    
-    // Transformer tout en miniscule*
-    const phraseMin = phrase.toLowerCase()
+    // Conversion de la phrase en minuscules
+    const phraseMinuscules = convertirEnMinuscules(phrase);
 
-    //Initialiser le nombre de voyelle
-    let nbrVoyelle = 0
+    // Appel de la fonction pour compter les voyelles
+    let nombreDeVoyelles = compterVoyelles(phraseMinuscules, voyelles);
 
-    //boucle pour parcourire chaque élément de la phrase
-    for(let i=0;i < phraseMin.length ;i++){
-
-        // verifier si le caractère d'indice [i] est voyelle
-        if(voyelle.includes(phraseMin[i])){
-
-            //Incrémenter le compteur de voyelle
-            nbrVoyelle ++;
-        }
-    }
-    return nbrVoyelle;
+    // Renvoie du nombre de voyelles
+    return nombreDeVoyelles;
 }
 
-const phrase = "Je ne suis pas malade pour le moment"
-console.log(getVoyelleNumber(phrase))
+// Fonction pour convertir une phrase en minuscules
+function convertirEnMinuscules(phrase) {
+    return phrase.toLowerCase();
+}
+
+// Fonction pour compter les voyelles dans une phrase
+function compterVoyelles(phraseMinuscules, voyelles) {
+    // Initialisation du compteur de voyelles
+    let compteurVoyelles = 0;
+
+    // Boucle pour parcourir chaque caractère de la phrase
+    for (let i = 0; i < phraseMinuscules.length; i++) {
+        // Vérification si le caractère courant est une voyelle
+        if (estVoyelle(phraseMinuscules[i], voyelles)) {
+            // Incrémentation du compteur de voyelles
+            compteurVoyelles++;
+        }
+    }
+
+    // Renvoie du nombre total de voyelles dans la phrase
+    return compteurVoyelles;
+}
+
+// Fonction pour vérifier si un caractère est une voyelle
+function estVoyelle(caractere, voyelles) {
+    return voyelles.includes(caractere);
+}
+
+// Phrase à analyser
+const phrase = "Je ne suis pas malade pour le moment";
+
+// Appel de la fonction principale et affichage du résultat
+console.log(getVoyelleNumber(phrase));
