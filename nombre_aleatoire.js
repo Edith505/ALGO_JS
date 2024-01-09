e8abbfb4ed68a82a62797047fbc7c8de7fe857f9
Edith Rand <edithrand505@gmail.com>
2024-01-09 18:24:20 +0100

    readme file

--- nombre_aleatoire.js
+++ nombre_aleatoire.js
@@ -1,7 +1,23 @@
-
-function nbrAleatoire(){
-    const random = Math.round(Math.random() * 10)
-    return random
+function generateRandomNumber() {
+    // Math.floor pour obtenir un nombre entier entre 0 (inclus) et 10 (inclus)
+    const random = Math.floor(Math.random() * 11);
+    return random;
 }
 
-console.log(nbrAleatoire())
+// Exemple d'utilisation
+console.log(generateRandomNumber());
+
+
+function generateRandomNumber2() {
+    // Utilisation de Math.random() pour obtenir un nombre décimal entre 0 (inclus) et 1 (exclus)
+    const randomNumber = Math.random();
+
+    // Multiplication par 11 pour obtenir un nombre entre 0 (inclus) et 10 (exclus)
+    const result = Math.floor(randomNumber * 11);
+
+    return result;
+}
+
+// Exemple d'utilisation
+const randomNum = generateRandomNumber2();
+console.log(randomNum);
