39cc4b2f3dde053c88d6a1980516d13e94f980a3
Edith Rand <edithrand505@gmail.com>
2024-01-09 18:19:12 +0100

    nombre aleatoir

--- Abreviation.js
+++ Abreviation.js
@@ -2,9 +2,12 @@ function abreviation(nomComplet) {
     
     // Séparer le prénom et le nom
     const parties = nomComplet.split(' ');
+
     // Récupérer le prénom et l'initial du nom
     const prenom = parties[0];
+
     const initialNom = parties[1].charAt(0).toUpperCase();  
+    
     // Retourner l'abréviation
     return `${prenom} ${initialNom}.`;
 }
---
+++ nombre_aleatoire.js
@@ -0,0 +1,7 @@
+
+function nbrAleatoire(){
+    const random = Math.round(Math.random() * 10)
+    return random
+}
+
+console.log(nbrAleatoire())
